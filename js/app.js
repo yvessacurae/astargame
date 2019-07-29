f88d24ecd81a99898da98486082d73988335bef0
Yves Sacurae <yvessacurae@gmail.com>
2019-07-28 23:20:49 -0300

    Add elemets to app

--- js/app.js
+++ js/app.js
@@ -3,4 +3,7 @@ import Tiles from './Tiles';
 const tiles = new Tiles();
 tiles.init();
 
-console.log(tiles.tileMap, '12');
+const tileMap = `<div>${tiles.tileMap}</div>`;
+console.log(tiles.tileMap)
+
+document.querySelector('#app').innerHTML = tileMap;
